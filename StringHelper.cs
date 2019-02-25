1a3232f75cb904fb36df3f00b0f3f2c26889da1b
VictoriaIL <47503735+VictoriaIL@users.noreply.github.com>
2019-02-25 17:21:26 +0300

    try1

--- StringHelper.cs
+++ StringHelper.cs
@@ -6,8 +6,27 @@ namespace PadawansTask4
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if ( str == "")
+            {
+                throw new ArgumentException();
+            }
+            
+            char[] arr = str.ToCharArray();
+            char[] glas = "aeoui".ToCharArray();
+            int count = 0;
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                for (int j = 0; j < glas.Length; ++j)
+                {
+                    if (arr[i] == glas[j])
+                        count++;
+                }
+            }
+            return count;
         }
     }
 }
